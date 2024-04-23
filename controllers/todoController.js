33cc2ac697969fb1a5c9fa1d35be6140673f872d
Acha-Goodness <acharious1668@gmail.com>
2024-04-23 23:39:50 +0100

    app controller configuration

---
+++ controllers/todoController.js
@@ -0,0 +1,14 @@
+const Todo = require("../Models/todoModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.createTodo = catchAsync( async (req, res, next) => {
+
+});
+
+exports.getAllTodo = catchAsync( async (req, res, next) => {
+
+});
+
+exports.deleteTodo = catchAsync( async (req, res, next) => {
+
+});
