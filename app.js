acecb46cac547644d1a1d133c6f3e6c8aef2a1cc
Acha-Goodness <acharious1668@gmail.com>
2024-04-23 23:34:19 +0100

    todo router configuration

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const todoRouter = require("./Routes/todoRoutes")
 
 const app = express();
 app.use(express.json());
@@ -14,5 +15,8 @@ if(process.env.NODE_ENV === "development"){
     app.use(morgan("dev"));
     console.log("My application is currently on", process.env.NODE_ENV);
 }
+
+// ENDPOINT ROUTING BY MOUNTING e.g mounting the router
+app.use("/api/v1/todo", todoRouter);
 
 module.exports = app;
