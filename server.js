b3db676d57bad16be7c7007ab12488fabaab2d85
Acha-Goodness <acharious1668@gmail.com>
2024-04-24 21:33:56 +0100

    Connected todo app to Atlas, mongoDB cloud version

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ process.on("uncaughtException", err => {
 dotenv.config({ path: "./config.env"});
 const app = require("./app");
 
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB)
-.then(res => {
+.then(() => {
     console.log("DB connection sucessful");
 }).catch(err => {
     console.log(err);
